19f93a538be6dc3d47cc2ccb1fd3fa6e8e9c0ba9
Artur Manukian <manukian.artur.94@gmail.com>
2019-05-24 12:50:04 +0300

    Add logic for hiding marker menu in MapComponent.

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -15,6 +15,7 @@ export class MapComponent implements OnInit {
   parentDataMatListIcon: string;
   childDataMatListIcon: string;
   activeMatListIcon: string;
+  isVisible: boolean;
   lt: number;
   lg: number;
   options: object;
@@ -28,6 +29,7 @@ export class MapComponent implements OnInit {
     this.parentDataMatListIcon = 'arrow_forward_ios';
     this.childDataMatListIcon = 'subdirectory_arrow_right';
     this.activeMatListIcon = 'arrow_back_ios';
+    this.isVisible = true;
     this.lt = 48.879966;
     this.lg = 11.726909;
     this.options = {
@@ -42,9 +44,13 @@ export class MapComponent implements OnInit {
     this.ACTIVE = [];
   }
 
-  addMarker() {
+  addMarker(id: number): void {
     const newMarker: Marker = marker([ this.lt + 5 * (Math.random() - 0.5), this.lg + 5 * (Math.random() - 0.5) ]);
     this.layers.push(newMarker);
+  }
+
+  handleMenu(): void {
+    this.isVisible = !this.isVisible;
   }
 
 }
